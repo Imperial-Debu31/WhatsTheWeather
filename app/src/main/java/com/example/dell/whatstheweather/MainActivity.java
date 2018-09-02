47f0376ef7658321ff948e59873408a507525851
Imperial-Debu31 <dbgoyal31@gmail.com>
2018-09-03 00:15:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/whatstheweather/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.dell.whatstheweather;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView resultTextView;
+
+    public void getWeather(View view)
+    {
+        try
+        {
+            DownloadTask task = new DownloadTask();
+            String encodedCityName = URLEncoder.encode(editText.getText().toString(), "UTF-8");
+
+            task.execute("http://openweathermap.org/data/2.5/weather?q=" + encodedCityName + "&appid=b6907d289e10d714a6e88b30761fae22");
+
+            InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            mgr.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Couldn't find the weather:(",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        resultTextView = findViewById(R.id.resultTextView);
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>
+    {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try
+            {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1)
+                {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return  result;
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+
+                Toast.makeText(getApplicationContext(),"Couldn't find the weather:(",Toast.LENGTH_SHORT).show();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try
+            {
+                JSONObject jsonObject = new JSONObject(s);
+
+                String weatherInfo = jsonObject.getString("weather");
+
+                Log.i("Weather Content", weatherInfo);
+
+                String msg = "";
+
+                JSONArray array = new JSONArray(weatherInfo);
+
+                for (int i=0; i < array.length(); i++)
+                {
+                    JSONObject jsonPart = array.getJSONObject(i);
+
+                    String main = jsonPart.getString("main");
+                    String description = jsonPart.getString("description");
+
+                    if (!main.equals("") && !description.equals(""))
+                    {
+                        msg += main + ": " + description + "\r\n";
+                    }
+                }
+
+                if (!msg.equals("")) {
+                    resultTextView.setText(msg);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Couldn't find the weather:(",Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(getApplicationContext(),"Couldn't find the weather:(",Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
